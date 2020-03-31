e7c33764a3a792bd8623342cd7f8dd688b31e2d5
Elijah Ahlstrom <elijah.ahlstrom@gmail.com>
2020-03-31 16:23:21 -0400

    Added test module for most_played_position()

--- tests/test_most_played_position.py
+++ tests/test_most_played_position.py
@@ -1,26 +1,26 @@
-
 """
-dataset.py
-Mihaela
-March 11, 2020
+test_most_played_position.py
+Elijah Ahlstrom
+March 31, 2020
 """
-import csv
+
+import unittest
+from problems.transformdata import TransformData
 
 
-class TransformData():
+class TestMostPlayedPosition(unittest.TestCase):
     """
-    Data processing functionality
+    TestCase for the most_played_position method
     """
+    def setUp(self):
+        """
+        Define object tst of type TransformData
+        """
+        self.tst = TransformData()
 
-    def __init__(self):
-        """
-        Creates and initializes the class attributes.
-        Attributes: three parallel lists
-            names: list of strings
-            salaries: list of integers
-            positions: list of 2-character strings
-        """
-        self.names = [
+
+    def test_league(self):
+        self.tst.names = [
             'Stephen Curry', 'Russell Westbrook', 'Chris Paul', 'James Harden',
             'Blake Griffin', 'Gordon Hayward', 'Kyle Lowry', 'Paul George',
             'Mike Conley', 'Kevin Durant', 'Paul Millsap', 'Al Horford',
@@ -36,7 +36,7 @@ class TransformData():
             'Tyler Johnson', 'John Wall', 'Jeff Teague', 'George Hill',
             'Klay Thompson', 'Enes Kanter', 'Wesley Matthews'
         ]
-        self.salaries = [
+        self.tst.salaries = [
             37457154, 35654150, 35654150, 35650150, 32088932, 31214295,
             31200000, 30560700, 30521115, 30000000, 29230769, 28928709,
             27977689, 27739975, 26011913, 25976111, 25759766, 25467250,
@@ -47,50 +47,19 @@ class TransformData():
             19500000, 19245370, 19169800, 19000000, 19000000, 18988725,
             18622514, 18622514
         ]
-        self.positions = [
+        self.tst.positions = [
             'PG', 'PG', 'PG', 'PG', 'PF', 'SF', 'PG', 'SF', 'PG', 'SF', 'PF',
             'PF', 'PG', 'SG', 'SF', 'PG', 'SG', 'PF', 'SF', 'SG', 'PF', 'C',
             'C', 'C', 'PF', 'C', 'PF', 'SF', 'SF', 'SG', 'C', 'SF', 'C', 'PF',
             'PF', 'PF', 'SF', 'SG', 'SG', 'PF', 'PG', 'PF', 'PG', 'SG', 'PG',
             'PG', 'PG', 'SG', 'C', 'SG'
         ]
-
-    def record_per_row(self):
-        """
-        Writes a CSV file with as many records as the size of any of the lists
-        A record is a row in the file, with three columns, corresonding to
-        a name, salary, and position.
-        """
-        with open('nba.csv', 'w') as nba_file:
-            for nme, slr, pos in zip(self.names, self.salaries, self.positions):
-                nsp_row = nme + ',' + str(slr) + ',' + pos + '\n'
-                nba_file.write(nsp_row)
-
-
-    def names_by_pos(self):
-        """
-        Returns a dictionary with the keys are set to the names of the positions
-        and the values are set to lists of each name that is that position
-        """
-        sort_by_pos={}
-        for nme, pos in zip(self.names, self.positions):
-            if pos in sort_by_pos.keys():
-                sort_by_pos[pos].append(nme)
-            else:
-                sort_by_pos[pos]=[nme]
-        return sort_by_pos
-
-
-    def most_played_position(self):
-        """
-        Calls names_by_pos(), and uses the returned dictionary to find the
-        number of players in each position, before returning a dictionary with
-        the key being the position and the value being the number of players
-        with that position
-        """
-        
+        actual_result = self.tst.most_played_position()
+        expected_result = {
+            'PG' : 13, 'PF' : 12, 'SF' : 9, 'SG' : 9, 'C' : 7
+        }
+        self.assertEqual(actual_result, expected_result)
 
 
 if __name__ == '__main__':
-    TD = TransformData()
-    TD.record_per_row()
+    unittest.main()
